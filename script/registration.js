5bf648d7b2aaf31dc07fde35ef6145c6956d9643
DE-TOX <djknight0140@gmail.com>
2024-02-11 10:07:21 +0530

    [DIVYANSH] ADDED: GET and DELETE Method

--- script/registration.js
+++ script/registration.js
@@ -18,7 +18,7 @@ function subDetails() {
         gender: gender ? "male" : "female",
         department: [],
         salary: salary,
-        imageSrc: [],
+        imageSrc: "" ,
         // date : date,
         note: note
     }
@@ -38,25 +38,23 @@ function subDetails() {
     if (department5) {
         arr.push("Others")
     }
-    let arr2;
+    let proImage;
     if (image1) {
-        arr2 = "../assests/WhatsApp Image 2024-02-09 at 15.48.53_9736ba1b.jpg"
+        proImage = "../assests/I1.jpg"
     }
-    //     arr2.push("../assests/WhatsApp Image 2024-02-09 at 15.48.53_9736ba1b.jpg");
-    // }
-    // if (image2) {
-    //     arr2.push("../assests/WhatsApp Image 2024-02-09 at 15.48.58_15abe770.jpg")
-    // }
-    // if (image3) {
-    //     arr2.push("../assests/WhatsApp Image 2024-02-09 at 15.49.23_5ff7c590.jpg")
-    // }
-    // if (image4) {
-    //     arr2.push("../assests/WhatsApp Image 2024-02-09 at 15.49.23_ae802d2b.jpg")
-    // }
+    if (image2) {
+        proImage = "../assests/I2.jpg"
+    }
+    if (image3) {
+        proImage = "../assests/I3.jpg"
+    }
+    if (image4) {
+        proImage = "../assests/I4.jpg"
+    }
 
 
     obj.department = arr;
-    obj.imageSrc = arr2;
+    obj.imageSrc = proImage
     $.ajax({
         url: "http://localhost:3000/data",
         method: "POST",
@@ -70,36 +68,35 @@ function subDetails() {
             console.log("Server response:", xhr.responseText);
         }
     });
-
 }
 
-function deleteRecord(obj) {
+
+function deleteRecord(id) {
     $.ajax({
-        url: "http://localhost:3000/data",
-        method: "delete",
-        data: JSON.stringify(obj),
-        contentType: "application/json",
+        url: "http://localhost:3000/data/" + id,
+        method: "DELETE",
         success: function (data) {
             console.log("Success:", data);
+            // Remove the deleted row from the table
+            $('#row_' + id).remove();
         },
         error: function (xhr, status, error) {
-            console.error("Error fetching data:", status, error);
+            console.error("Error deleting record:", status, error);
             console.log("Server response:", xhr.responseText);
         }
     });
 }
+
+
 $(document).ready(function () {
     $.ajax({
         url: 'http://localhost:3000/data',
         type: 'GET',
         success: function (data) {
-            console.log(data);
-
             data.forEach(element => {
-                //console.log(element.imageSrc);
-                var nRow = "<tr><td>" + element.name + "</td><td>" + element.gender + "</td><td>" +
-                    element.department + "</td><td>" + element.salary + "</td><td>" + element.note + "</td><td><button>🗑️</button><button>📝</button>"
-                $("#table").append(nRow)
+                var nRow = "<tr id='row_" + element.id + "'><td>" +"<img class = 'proImg' src ="+element.imageSrc+">"+element.name + "</td><td>" + element.gender + "</td><td>" +
+                    element.department + "</td><td>" + element.salary + "</td><td>" + element.note + "</td><td><button class='delete-btn'>🗑️</button><button class='edit-btn'>📝</button></td></tr>";
+                $("#table").append(nRow);
             });
         },
         error: function (xhr, status, error) {
@@ -107,6 +104,11 @@ $(document).ready(function () {
         }
     })
 })
+$(document).on('click', '.delete-btn', function() {
+    var rowId = $(this).closest('tr').attr('id');
+    var id = rowId.split('_')[1];
+    deleteRecord(id);
+});
 
 
 
